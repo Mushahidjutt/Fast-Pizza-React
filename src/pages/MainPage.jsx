86548d4926be09868f5fb3c1494f3ca7823a6bce
Mushahidjutt <mushahidbuttar352@gmail.com>
2025-08-31 16:33:43 +0500

    Add Headings

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -8,14 +8,6 @@ export default function MainPage() {
       <Header />
       <main className="mx-auto max-w-3xl tracking-widest">
         <div className="my-10 px-4 text-center sm:my-16">
-          <h1 className="mb-8  text-xl font-semibold md:text-3xl">
-            The best pizza.
-            <br />
-            <span className="text-yellow-500">
-              Straight out of the oven, straight to you.
-            </span>
-          </h1>
-
           <p className="mb-4 text-sm text-stone-600 md:text-base">
             {" "}
             👋 Welcome! Please start by telling us your name:
